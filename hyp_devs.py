6385b23bc05e0d32016615372244c18534694e9f
ligasa <109149894+ligasa@users.noreply.github.com>
2023-05-18 18:19:52 +0200

    Update hyp_devs.py

--- hyp_devs.py
+++ hyp_devs.py
@@ -54,30 +54,3 @@ button_html = f'''
 '''
 
 st.markdown(button_html, unsafe_allow_html=True)
-
-def hide_anchor_link():
-    st.markdown(
-        body="""
-        <style>
-            h1 > div > a {
-                display: none;
-            }
-            h2 > div > a {
-                display: none;
-            }
-            h3 > div > a {
-                display: none;
-            }
-            h4 > div > a {
-                display: none;
-            }
-            h5 > div > a {
-                display: none;
-            }
-            h6 > div > a {
-                display: none;
-            }
-        </style>
-        """,
-         unsafe_allow_html=True,
-)
