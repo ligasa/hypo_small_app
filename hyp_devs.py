cabe22662dbdcde67ce0038179d020e948395e23
ligasa <109149894+ligasa@users.noreply.github.com>
2023-05-19 18:31:44 +0200

    Update hyp_devs.py

--- hyp_devs.py
+++ hyp_devs.py
@@ -4,9 +4,23 @@ import streamlit as st
 logo_image = "icon calc/finance.e15.cz_logo_500_DPI_edit.png"  # Nahraďte cestou k úvodnímu logu ve formátu PNG nebo JPG
 st.image(logo_image)
 
+# Funkce pro aktualizaci URL
+def update_url():
+    global url_link
+    url_link = f"https://prodej.e15.cz/hypoteky/srovnani/?loan={loan_value}&type={selected_type}"
+    
 # Vstupní pole pro hodnotu hypotéky
 st.markdown("**HODNOTA HYPOTÉKY (v Kč)**")
-loan_value = st.number_input("HODNOTA HYPOTÉKY", value=0, step=1, format="%d", key="loan_value", label_visibility="collapsed")
+loan_value = st.text_input("HODNOTA HYPOTÉKY", value="", key="loan_value", label_visibility="collapsed")
+
+# Odstranění mezer z vstupního řetězce
+loan_value = loan_value.replace(" ", "")
+
+# Ověření, zda je vstup číslo
+if loan_value.isnumeric():
+    loan_value = int(loan_value)
+else:
+    loan_value = 0
 
 # Změna barvy vstupního pole
 loan_value_style = """
@@ -28,33 +42,28 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 type_mapping = {"Dům": "0", "Byt": "1", "Nevím": "2"}
 selected_type = type_mapping[property_type]
 
-# Funkce pro aktualizaci URL
-def update_url():
-    global url_link
-    url_link = f"https://prodej.e15.cz/hypoteky/srovnani/?loan={loan_value}&type={selected_type}"
-    
-    button_html = f'''
-        <a href="{url_link}">
-            <button style="
-                fontWeight: 400;
-                padding: 0.25rem 0.75rem;
-                borderRadius: 0.25rem;
-                margin: 0px;
-                lineHeight: 1.6;
-                width: auto;
-                userSelect: none;
-                backgroundColor: #FA595D;
-                color: #FFFFFF;
-                border: 1px solid rgba(49, 51, 63, 0.2);
-                border-color: #FFFFFF"
-            >
-                {'Spočítat nejlepší nabídky'}
-            </button>
-        </a>
-    '''
+# Inicializace URL
+update_url()
 
-    st.markdown(button_html, unsafe_allow_html=True)
+# Tlačítko
+button_html = f'''
+    <a href="{url_link}">
+        <button style="
+            fontWeight: 400;
+            padding: 0.25rem 0.75rem;
+            borderRadius: 0.25rem;
+            margin: 0px;
+            lineHeight: 1.6;
+            width: auto;
+            userSelect: none;
+            backgroundColor: #FA595D;
+            color: #FFFFFF;
+            border: 1px solid rgba(49, 51, 63, 0.2);
+            border-color: #FFFFFF"
+        >
+            {'Spočítat nejlepší nabídky'}
+        </button>
+    </a>
+'''
 
-# Při změně hodnoty vstupního pole hypotéky nebo typu nemovitosti se automaticky aktualizuje URL
-if loan_value or property_type:
-    update_url()
+st.markdown(button_html, unsafe_allow_html=True)
