c1bcb2ef60968eb4f2bb8191085f42245a0a64c7
ligasa <109149894+ligasa@users.noreply.github.com>
2023-05-19 10:31:28 +0200

    Update hyp_devs.py

--- hyp_devs.py
+++ hyp_devs.py
@@ -13,7 +13,7 @@ st.markdown(
 
 # Vložení úvodního loga
 logo_image = "icon calc/finance.e15.cz_logo_500_DPI.png"  # Nahraďte cestou k úvodnímu logu ve formátu PNG nebo JPG
-st.image(logo_image, width=200)
+st.image(logo_image)
 
 # Vstupní pole pro hodnotu hypotéky
 st.markdown("**HODNOTA HYPOTÉKY (v Kč)**")
