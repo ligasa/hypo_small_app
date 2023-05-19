b928de37a1bc025867c248c73c80510719373fba
ligasa <109149894+ligasa@users.noreply.github.com>
2023-05-19 13:54:48 +0200

    Update hyp_devs.py

--- hyp_devs.py
+++ hyp_devs.py
@@ -6,7 +6,7 @@ st.image(logo_image, width=200)
 
 # Vstupní pole pro hodnotu hypotéky
 st.markdown("**HODNOTA HYPOTÉKY (v Kč)**")
-loan_value = st.text_input("", value="", key="loan_value",label_visibility="collapsed")
+loan_value = st.text_input("", value="", key="loan_value", label_visibility="collapsed")
 
 # Odstranění mezer z vstupního řetězce
 loan_value = loan_value.replace(" ", "")
@@ -26,7 +26,7 @@ st.markdown(loan_value_style, unsafe_allow_html=True)
 
 # Radio button pro výběr typu nemovitosti
 st.markdown("**TYP NEMOVITOSTI**")
-property_type = st.radio("TYP NEMOVITOSTI", ["Dům", "Byt", "Nevím"],label_visibility="collapsed")
+property_type = st.radio("TYP NEMOVITOSTI", ["Dům", "Byt", "Nevím"], label_visibility="collapsed")
 
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
@@ -40,6 +40,28 @@ def generate_url(loan_value, selected_type):
 
 url_link = generate_url(loan_value, selected_type)
 
+button_html = f'''
+    <a href="{url_link}">
+        <button style="
+            fontWeight: 400;
+            padding: 0.25rem 0.75rem;
+            borderRadius: 0.25rem;
+            margin: 0px;
+            lineHeight: 1.6;
+            width: auto;
+            userSelect: none;
+            backgroundColor: #FA595D;
+            color: #FFFFFF;
+            border: 1px solid rgba(49, 51, 63, 0.2);
+            border-color: #FFFFFF"
+        >
+            {'Spočítat nejlepší nabídky'}
+        </button>
+    </a>
+'''
+
+# Aktualizace URL při každé změně hodnoty vstupů
+url_link = generate_url(loan_value, selected_type)
 button_html = f'''
     <a href="{url_link}">
         <button style="
