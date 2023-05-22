987b79a3cb3936a37dc95326cee37412567471c5
ligasa <109149894+ligasa@users.noreply.github.com>
2023-05-22 17:06:33 +0200

    Update hyp_devs.py

--- hyp_devs.py
+++ hyp_devs.py
@@ -1,9 +1,21 @@
 import streamlit as st
 
-def update_url():
+
+def update_url_and_open_page():
     global url_link
     url_link = f"https://prodej.e15.cz/hypoteky/srovnani/?loan={loan_value}&type={selected_type}"
     st.session_state.selected_type = selected_type
+    open_page(url_link)
+
+
+def open_page(url):
+    open_script = f"""
+        <script type="text/javascript">
+            window.open('{url}', '_blank').focus();
+        </script>
+    """
+    st.components.v1.html(open_script)
+
 
 # Vložení úvodního loga
 logo_image = "icon calc/finance.e15.cz_logo_500_DPI_edit.png"  # Nahraďte cestou k úvodnímu logu ve formátu PNG nebo JPG
@@ -11,7 +23,7 @@ st.image(logo_image)
 
 # Vstupní pole pro hodnotu hypotéky
 st.markdown("**HODNOTA HYPOTÉKY (v Kč)**")
-loan_value = st.text_input("HODNOTA HYPOTÉKY", value="", key="loan_value", label_visibility="collapsed", on_change=update_url)
+loan_value = st.text_input("HODNOTA HYPOTÉKY", value="", key="loan_value", label_visibility="collapsed")
 
 # Odstranění mezer z vstupního řetězce
 loan_value = loan_value.replace(" ", "")
@@ -31,7 +43,8 @@ st.markdown(loan_value_style, unsafe_allow_html=True)
 
 # Radio button pro výběr typu nemovitosti
 st.markdown("**TYP NEMOVITOSTI**")
-property_type = st.radio("TYP NEMOVITOSTI", ["Dům", "Byt", "Nevím"], label_visibility="collapsed", on_change=update_url)
+property_type = st.radio("TYP NEMOVITOSTI", ["Dům", "Byt", "Nevím"], label_visibility="collapsed")
+
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 # Přiřazení číselné hodnoty typu nemovitosti
@@ -41,33 +54,6 @@ selected_type = type_mapping[property_type]
 # Přiřazení selected_type do session_state
 st.session_state.selected_type = selected_type
 
-
-import streamlit as st
-
-# ...
-
-if loan_value > 0:
-    url_link = f"https://prodej.e15.cz/hypoteky/srovnani/?loan={loan_value}&type={selected_type}"
-    button_html = f'''
-        <a href="{url_link}">
-            <button style="
-                fontWeight: 400;
-                padding: 0.25rem 0.75rem;
-                borderRadius: 0.25rem;
-                margin: 0px;
-                lineHeight: 1.6;
-                width: auto;
-                userSelect: none;
-                backgroundColor: #FA595D;
-                color: #FFFFFF;
-                border: 1px solid rgba(49, 51, 63, 0.2);
-                border-color: #FFFFFF"
-                onclick="this.disabled=true"
-            >
-                {'Spočítat nejlepší nabídky'}
-            </button>
-        </a>
-    '''
-    st.markdown(button_html, unsafe_allow_html=True)
-else:
-    st.markdown("**Zadejte hodnotu hypotéky pro výpočet**")
+# Button to calculate the best offers and navigate to the URL
+button_text = 'Spočítat nejlepší nabídky'
+st.button(button_text, on_click=update_url_and_open_page)
